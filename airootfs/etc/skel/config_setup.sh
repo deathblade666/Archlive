d0d52f9371e63af4193379fbe83ff471d88066e5
deathblade666 <halo2ofdeath@gmail.com>
2020-06-02 18:34:11 -0400

    Update config_setup.sh

--- airootfs/etc/skel/config_setup.sh
+++ airootfs/etc/skel/config_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## cloning zsh config
+# cloning and installing oh-my-zsh and powerline fonts 
 git clone https://github.com/deathblade666/zsh.git ~/.config/zsh
 cd ~/.config/zsh/script
 chmod +x install_zsh.sh
@@ -10,18 +10,27 @@ cp ~/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
 mv ~/.zshrc ~/zshrc.bak
 cp ~/.config/zsh/.zshrc ~/.zshrc
 
-## If running i3 Window Manager uncomment the following line
+# clone i3 config
 git clone https://github.com/deathblade666/i3config.git ~/.config/i3config
 
-## if running KDE and want i3 to be your window manager ucomment the following line
+# clone config files
 git clone https://github.com/deathblade666/kde-i3-setup.git ~/.config/kde-i3
 
-# uncomment the following lines for individual application configs
+# copy config files and remove old directories
 cp -r ~/.config/i3config/i3 ~/.config/i3
 cp -r ~/.config/kde-i3/rofi ~/.config/rofi
 rm -r ~/.config/i3/i3conifg/ <<< yes <<< yes
 rm -r ~/.config/kde-i3/ <<< yes <<< yes
 
+# Set wallpaper
+mkdir ~/wallpaper
+cp /config/arch-wallpaper.png ~/wallpaper/arch-wallpaper.png
+mkdir ~/.config
+cd .config
+mkdir nitrogen
+cp /config/bg-save.cfg /home/deathmasia/.config/nitrogen/bg-save.cfg
+
+# install yay and st terminal
 git clone https://aur.archlinux.org/yay.git ~/yay
 cd ~/yay
 makepkg -si
