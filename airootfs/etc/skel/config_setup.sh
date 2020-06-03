3a5b8a3af2d7d4c3b0c04b40773671e836e93359
deathblade666 <halo2ofdeath@gmail.com>
2020-06-03 16:58:52 -0400

    Update config_setup.sh

--- airootfs/etc/skel/config_setup.sh
+++ airootfs/etc/skel/config_setup.sh
@@ -18,17 +18,10 @@ git clone https://github.com/deathblade666/kde-i3-setup.git ~/.config/kde-i3
 
 # copy config files and remove old directories
 cp -r ~/.config/i3config/i3 ~/.config/i3
+cp -r ~/.config/kde-i3/nitrogen ~/.config/nitrogen
 cp -r ~/.config/kde-i3/rofi ~/.config/rofi
 rm -r ~/.config/i3conifg/ <<< yes <<< yes
 rm -r ~/.config/kde-i3/ <<< yes <<< yes
-
-# Set wallpaper
-mkdir ~/wallpaper
-cp /config/arch-wallpaper.png ~/wallpaper/arch-wallpaper.png
-mkdir ~/.config
-cd .config
-mkdir nitrogen
-cp /config/bg-save.cfg /home/deathmasia/.config/nitrogen/bg-save.cfg
 
 # install yay and st terminal
 git clone https://aur.archlinux.org/yay.git ~/yay
