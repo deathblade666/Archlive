77884c6fef713032fe1f0ae08287d94b251c7a12
deathblade666 <halo2ofdeath@gmail.com>
2020-06-04 01:19:18 +0000

    Update config_setup.sh

--- airootfs/etc/skel/config_setup.sh
+++ airootfs/etc/skel/config_setup.sh
@@ -20,7 +20,7 @@ git clone https://github.com/deathblade666/kde-i3-setup.git ~/.config/kde-i3
 cp -r ~/.config/i3config/i3 ~/.config/i3
 cp -r ~/.config/kde-i3/nitrogen ~/.config/nitrogen
 cp -r ~/.config/kde-i3/rofi ~/.config/rofi
-rm -r ~/.config/i3conifg/ <<< yes <<< yes
+rm -r ~/.config/i3config/ <<< yes <<< yes
 rm -r ~/.config/kde-i3/ <<< yes <<< yes
 
 # install yay and st terminal
@@ -28,3 +28,8 @@ git clone https://aur.archlinux.org/yay.git ~/yay
 cd ~/yay
 makepkg -si
 yay -S st <<< N
+yay -S preload <<< N
+systemctl enable preload
+wifi-menu
+clear
+ech Setup complete you may now start using this system as normal, to get a UI type "startx"
