e6b7467cf943ed3dd5310a002f6f4a47ec0e3a39
deathblade666 <halo2ofdeath@gmail.com>
2020-06-20 16:17:18 +0000

    remove the removal of the config directories from repo as they are now set as the configs to use

--- airootfs/etc/skel/config_setup.sh
+++ airootfs/etc/skel/config_setup.sh
@@ -12,18 +12,6 @@ cp ~/.config/dot_files/zsh/oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
 mv ~/.zshrc ~/zshrc.bak
 cp ~/.config/dot_files/zsh/.zshrc ~/.zshrc
 
-# clone i3 config
-mv -r ~/.config/dot_files/i3 ~/.config/i3
-mv -r ~/.config/dot_files/rofi ~/.config/rofi
-mv -r ~/.config/dot_files/i3blocks ~/.config/i3blocks
-
-# copy config files and remove old directories
-cp -r ~/.config/i3config/i3 ~/.config/i3
-cp -r ~/.config/kde-i3/nitrogen ~/.config/nitrogen
-cp -r ~/.config/kde-i3/rofi ~/.config/rofi
-rm -r ~/.config/i3config/ <<< yes <<< yes
-rm -r ~/.config/kde-i3/ <<< yes <<< yes
-
 # install yay and st terminal
 git clone https://aur.archlinux.org/yay.git ~/yay
 cd ~/yay
