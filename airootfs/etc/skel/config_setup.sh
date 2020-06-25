2799088f979cb1c1b8561008fd1b5ab98c355c22
deathblade666 <halo2ofdeath@gmail.com>
2020-06-24 21:16:08 -0400

    fixed invalid directory

--- airootfs/etc/skel/config_setup.sh
+++ airootfs/etc/skel/config_setup.sh
@@ -8,8 +8,8 @@ cd ~/.config/dot_files/zsh/script
 chmod +x install_zsh.sh
 ./install_zsh.sh
 mv ~/.oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh.bak
-cp ~/.config/dot_files/zsh/oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
-mv ~/.zshrc ~/zshrc.bak
+cp ~/.config/zsh/oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
+mv ~/.zshrc ~/.zshrc.bak
 cp ~/.config/dot_files/zsh/.zshrc ~/.zshrc
 
 # install yay and st terminal
