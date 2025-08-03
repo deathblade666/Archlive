41bdd288b453016d42d2dd76897858c16900d8c6
deathmasia <halo2ofdeath@gmail.com>
2025-08-03 16:27:57 -0400

    adjusted to use systemd boot, homed, networkd, and resolved.

--- airootfs/etc/chroot_install.sh
+++ airootfs/etc/chroot_install.sh
@@ -2,8 +2,9 @@
 
 # Add prompts for locale 
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
 echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
-timedatectl set-timezone America/New_York
+#timedatectl set-timezone America/New_York
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
@@ -17,6 +18,35 @@ echo 127.0.0.1 localhost > /etc/hosts
 echo ::1 localhost >> /etc/hosts
 echo 127.0.1.1 $Hostname >> /etc/hosts
 clear
+
+# Stage default network config
+eth_device=$(ip -o link show | awk -F': ' '{print $2}' | grep -E '^e(n|th|np)')
+#wifi_device=$(ip -o link show | awk -F': ' '{print $2}' | grep -E '^e(n|th|np)')
+cat > /etc/systemd/network/20-wired.network << EOF
+[Match]
+Name=$eth_device
+
+[Link]
+RequiredForOnline=routable
+
+[Network]
+DHCP=yes
+EOF 
+
+#cat > /etc/systemd/network/ << EOF
+#[Match]
+#Name=$wifi_device
+#
+#[Link]
+#RequiredForOnline=routable
+#
+#[Network]
+#DHCP=yes
+#IgnoreCarrierLoss=3s
+#EOF
+
+mkinitcpio -P
+clear
 echo enter new root password
 passwd
 
@@ -28,53 +58,64 @@ passwd
 # grub-mkconfig -o /boot/grub/grub.cfg
 
 # systemd-boot
-mount /dev/nvme0n1p1 /boot
 bootctl install
+# get UUID
+uuid=lsblk -dno UUID /dev/nvme0n1p3
+#mount /dev/nvme0n1p1 /boot
+cat > /boot/loader/loader.conf << EOF
+default  arch.conf
+timeout  4
+console-mode max
+editor   no
+EOF
+cat > /boot/loader/entries/arch.conf << EOF
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /initramfs-linux.img
+options root=UUID=$uuid rw
+EOF
+bootctl update
 
-pacman -S --noconfirm dhcpcd ebtables fakeroot sudo dnsmasq archiso libvirt virt-manager git curl wget alsa-utils netctl dialog wpa_supplicant polkit-gnome zsh go base-devel htop nfs-utils vi vim hsetroot neofetch xorg xorg-xinit qemu ovmf nvidia chromium steam alacritty discord qtile ranger docker pipewire pipwire-pulse pipewire-jack zathura pacmanfm qview ddclient obsidian  <<< Y
+
+#pacman -S --noconfirm dhcpcd ebtables fakeroot sudo dnsmasq archiso libvirt virt-manager git curl wget alsa-utils netctl dialog wpa_supplicant polkit-gnome zsh go base-devel htop nfs-utils vi vim hsetroot neofetch xorg xorg-xinit qemu ovmf nvidia chromium steam alacritty discord qtile ranger docker pipewire pipwire-pulse pipewire-jack zathura pacmanfm qview ddclient obsidian  <<< Y
 
 # Create user
+systemctl start systemd-homed
 clear
 echo enter user name
 read User
 echo "set default shell (zsh/bash)"
 read Setshell
-useradd -m -s /bin/$Setshell $User
-echo enter user password
-passwd $User
-clear
-echo "add $User to docker group? [y/n]"
-read docker
-if [$docker = "y"]
-then 
-  usermod -aG docker $User
-fi
 clear
 echo "add $User to sudoers file? [y/n]"
 read Allthethings
 if [$Allthethings = "y"]
 then 
-  echo '$User ALL=(ALL:ALL) ALL' >> /etc/sudoers
+  echo "$User ALL=(ALL:ALL) ALL" >> /etc/sudoers
 fi
+clear
+homectl create $User --storage=luks --shell=/bin/$Setshell --member-of=docker
 
-systemctl enable libvirtd --now
-systemctl enable dhcpcd --now
-systemctl enable docker --now
+# set services to run on boot
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+systemctl enable systemd-homed
+systemctl enable docker
 
 # mv /config/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf
 
-amixer sset Master unmute
-amixer sset Speaker unmute
+#amixer sset Master unmute
+#amixer sset Speaker unmute
 
 # install portainer
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+#docker volume create portainer_data
+#docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 # install dwm
-git clone https://github.com/deathblade666/dotfiles/.config/dwm /root/dwm
-git clone https://github.com/deathblade666/.config/scripts/dwm /home/$User/.config/scripts/dwm
-cd /root/dwm
-make clean install
+#git clone https://github.com/deathblade666/dotfiles/.config/dwm /root/dwm
+#git clone https://github.com/deathblade666/.config/scripts/dwm /home/$User/.config/scripts/dwm
+#cd /root/dwm
+#make clean install
 
 rm /etc/install.sh
 echo User creation and application installation is complete, you may now reboot into your new system.
--- airootfs/etc/install.sh
+++ airootfs/etc/install.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-# set up partitions: this sets a root partition and a EFI partition
-# EFI parition is set to 512MB
-(echo o; echo n; echo p; echo 1; echo""; echo +512M; echo t; echo ef; echo n; echo p; echo 2; echo ""; echo ""; echo w) | fdisk /dev/nvme0n1
+# Sync time
+timedatectl
+
+# set up partitions: this sets a root partition, SWAP partition (4G) and a EFI partition (1G)
+(echo o; echo n; echo p; echo 1; echo""; echo +1G; echo t; echo ef; echo n; echo p; echo 2; echo ""; echo 4G; echo n; echo p; echo 3; echo ""; echo ""; echo w) | fdisk /dev/nvme0n1
+
+#format Partitions
 mkfs.fat -F32 /dev/nvme0n1p1
-mkfs.ext4 /dev/nvme0n1p2 <<< Y
-mount /dev/nvme0n1p2 /mnt
-mkdir /mnt/boot
+mkfs.btrfs /dev/nvme0n1p3 <<< Y
+mkswap /dev/nvme0n1p2
+
+#mount Partitions
+mount /dev/nvme0n1p3 /mnt
+mount --mkdir /dev/nvme0n1p1 /mnt/boot
+swapon /dev/nvme0n1p2
 
 # update repos
 pacman -Syy
 
+# install reflector is not already and sort mirrors
+pacman -S reflector --noconfirm
+reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+
 # install base system
-pacstrap /mnt base linux linux-firmware vim
+pacstrap /mnt base-devel linux linux-firmware vim docker pipewire alacritty pipwire-pulse steam htop git curl wget zsh vi nvidia qview fakeroot pcmanfm wayland xorg-xwayland
 
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
-mkdir /mnt/config
 # cp /root/config_setup.sh /mnt/etc/skel/config_setup.sh
 # cp /root/.xinitrc /mnt/etc/skel/.xinitrc
-cp /etc/chroot_install.sh /mnt/config/chroot_install.sh
+cp /etc/chroot_install.sh /mnt/root/chroot_install.sh
 # cp /etc/post_install.sh /mnt/config/post_install.sh
-mv /mnt/etc/pacman.conf /mnt/etc/pacman.conf.bak
-cp /etc/pacman.conf /mnt/etc/pacman.conf
+#mv /mnt/etc/pacman.conf /mnt/etc/pacman.conf.bak
+#cp /etc/pacman.conf /mnt/etc/pacman.conf
 # cp /root/10-monitor.conf /mnt/config/10-monitor.conf
 # cp /root/arch-wallpaper.png /mnt/config/arch-wallpaper.png
-arch-chroot /mnt ./config/chroot_install.sh
+arch-chroot /mnt ./root/chroot_install.sh
